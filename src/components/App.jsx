4822cbaf180bfaa14a691cf0e4d8e4f2d8eb8793
MythlinMasterdo <miles71397@gmail.com>
2016-09-30 19:23:38 -0700

    Starting to fix last App test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,27 @@
-var App = (props) => {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoList: window.exampleVideoData,
+      currentVideo: window.exampleVideoData
+    };
+  }
 
-
-  console.log('window.exampleVideoData we pass into VideoPlayer: ', props);
-  console.log('window.exampleVideoData[0] we pass into VideoPlayer: ', window.exampleVideoData[0]);
-
-  return (
-    <div>
-      <Nav />
-      <div className="col-md-7">
-        <VideoPlayer video={window.exampleVideoData[0]}/>
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={window.exampleVideoData[0]}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={window.exampleVideoData} />
+        </div>
       </div>
-      <div className="col-md-5">
-        <VideoList videos={window.exampleVideoData} />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
